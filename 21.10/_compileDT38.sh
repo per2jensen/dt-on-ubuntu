6cb81d737cb09877e8e632bd90c57e0becfa6ef7
Per Jensen <per2jensen@gmail.com>
2021-12-29 09:47:56 +0100

    added libs: heif, portmidi and sdl2

--- 21.10/_compileDT38.sh
+++ 21.10/_compileDT38.sh
@@ -1,3 +1,5 @@
+LOG=DT38.log
+
 #########################
 # don't change below
 #########################
@@ -25,51 +27,60 @@ sudo apt install -y lld-12 llvm-12-dev llvm-12-runtime llvm-12 lldb-12 python3-l
    intltool cmake
 if [ $? != "0" ]
 then
-    echo "Package installation failed, exiting"  && exit
+    echo "1: Package installation failed, exiting"  && exit
 fi 
 
 sudo apt install -y  libxml2-utils xsltproc libsaxon-java libpthread-workqueue0 libpthread-workqueue-dev  \
    libc6-dev libglib2.0-dev   \
    liblensfun-dev libcurl4-openssl-dev libjpeg8-dev libtiff-dev liblcms2-dev libjson-glib-dev  \
    libgtk-3-0 libgtk-3-dev libxml2-dev libxml2 librsvg2-2 librsvg2-dev libsqlite3-dev  \
-   libexiv2-dev libpugixml-dev lua5.3 liblua5.3-0 liblua5.3-dev libgphoto2-6 libgphoto2-dev colord libcolord-dev  \
+   libexiv2-dev libpugixml-dev lua5.4 liblua5.4-0 liblua5.4-dev libgphoto2-6 libgphoto2-dev colord libcolord-dev  \
    libgraphicsmagick1-dev imagemagick libmagick++-6.q16-dev libopenexr-dev libopenexr25 desktop-file-utils libgmic-dev
 if [ $? != "0" ]
 then
-    echo "Package installation failed, exiting"  && exit
+    echo "2: Package installation failed, exiting"  && exit
 fi 
 
 sudo apt install -y  libflickcurl-dev libraptor2-dev libopenjp2-7-dev libopenjp2-7 libosmgpsmap-1.0-dev libosmgpsmap-1.0-1  \
    libcolord-gtk-dev libcolord-gtk1 libcups2-dev libcups2  \
    libavif-bin libavif-dev libavif-gdk-pixbuf  \
    libsecret-1-dev libsecret-1-0   \
-   libimage-exiftool-perl liblua5.4-0 liblua5.4-dev
+   libimage-exiftool-perl
 
 if [ $? != "0" ]
 then
-    echo "Package installation failed, exiting"  && exit
+    echo "3: Package installation failed, exiting"  && exit
 fi 
+
+sudo apt install -y  debhelper libheif-dev libportmidi-dev libsdl2-dev
+
+if [ $? != "0" ]
+then
+    echo "4: Package installation failed, exiting"  && exit
+fi 
+
+
 
 
 # get the source ready
 if [[ -d $DT_SRC_FOLDER  ]]; then
   echo "darktable dir exists"
   cd $DT_SRC_FOLDER
-  git pull --rebase
+  git pull --rebase|tee $LOG
 else
   mkdir -p $DT_SRC_FOLDER
-  git clone git://github.com/darktable-org/darktable.git $DT_SRC_FOLDER
+  git clone git://github.com/darktable-org/darktable.git $DT_SRC_FOLDER |tee $LOG
   cd $DT_SRC_FOLDER
 fi
-git checkout ${RELEASE}
-git submodule init
-git submodule update
+git checkout ${RELEASE}  |tee -a $LOG
+git submodule init  |tee -a $LOG
+git submodule update  |tee -a $LOG
 
 
 
 # build darktable
-sudo ./build.sh --clean-all
-./build.sh --prefix ${INSTALL_PREFIX}  | tee dt-build.log
+mv ${INSTALL_PREFIX} ${INSTALL_PREFIX}-org
+./build.sh --prefix ${INSTALL_PREFIX} |tee -a $LOG
 if [ $? != "0" ]
 then
     echo "Build failed, exiting"  && exit
@@ -78,11 +89,11 @@ fi
 
 
 #install darktable
-sudo cmake --build "$DT_SRC_FOLDER/build" --target install -- -j1
+sudo cmake --build "$DT_SRC_FOLDER/build" --target install -- -j1   |tee -a $LOG
 if [ $? != "0" ]
 then
     echo "Installation failed, exiting"  && exit
 fi
 
 # print --version info
-${INSTALL_PREFIX}/bin/darktable --version
+${INSTALL_PREFIX}/bin/darktable --version  |tee -a $LOG
