cf2e17ec67342c5fb739c10d719b8a6fc143a396
Per Jensen <per2jensen@gmail.com>
2023-06-11 12:41:52 +0200

    support for DT 4.3.0 on 22.04

---
+++ 22.04/DT43/DTcompile.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+##
+## Author: Per Jensen
+## Contact: per2jensen@gmail.com
+## Licensed under the Apache License 2.0
+##
+
+source envvars
+
+
+# Folder which already contains a darktable git checkout or should be used for a fresh one 
+export DT_SRC_FOLDER="$HOME/git/darktable"
+
+# the code name for Ubuntu used in LLVM's repos
+export CODENAME_LLVM="jammy"
+
+# shellcheck source=/dev/null
+source "./$_COMPILE"
