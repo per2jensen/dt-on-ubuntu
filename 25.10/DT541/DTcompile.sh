955aac8c0be3acf73e98f9d8683050d6ccabd503
Per Jensen <per2jensen@gmail.com>
2026-04-30 19:59:46 +0200

    DT541 on 25.10 + DT54 fix

---
+++ 25.10/DT541/DTcompile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+##
+## Author: Per Jensen
+## Contact: per2jensen@gmail.com
+## Licensed under the Apache License 2.0
+##
+
+source envvars
+
+# shellcheck source=/dev/null
+source "./$_COMPILE"
---
+++ 25.10/DT541/master_compile.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+##
+## Author: Per Jensen
+## Contact: per2jensen@gmail.com
+## Licensed under the Apache License 2.0
+##
+
+source envvars
+
+# Directory where darktable "master" is installed
+INSTALL_PREFIX="$HOME/programmer/darktable-master"
+
+# Folder which already contains a darktable git checkout or should be used for a fresh one 
+DT_SRC_FOLDER="$HOME/git/darktable"
+
+# the code name for Ubuntu used in LLVM's repos
+CODENAME_LLVM="noble"
+
+# use git master
+RELEASE=master
+
+# shellcheck source=/dev/null
+source "./$_COMPILE"
